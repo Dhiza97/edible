437a7f25e8854a242490907ed679b557ae7446c5
Joseph Garba <josephgarba97@gmail.com>
2025-02-03 03:27:49 +0100

    Create category section

--- src/app/assets/assets.js
+++ src/app/assets/assets.js
@@ -1,21 +1,46 @@
-import burg from "./burg.png";
 import logo_light from "./Edible1.png";
 import logo_dark from "./Edible2.png";
-import meal1 from "./meal1.png";
-import meal2 from "./pesto.png";
-import meal3 from "./pizza.png";
-import meal4 from "./smoothie.png";
 import parallax1 from "./parallax1.jpg";
 import parallax2 from "./parallax2.jpg";
+import { RiDrinks2Fill } from "react-icons/ri";
+import { IoFastFood } from "react-icons/io5";
+import { LuDessert } from "react-icons/lu";
+import { FaBowlFood } from "react-icons/fa6";
 
 export const assets = {
-  burg,
   logo_light,
   logo_dark,
-  meal1,
-  meal2,
-  meal3,
-  meal4,
   parallax1,
   parallax2,
 };
+
+export const category = [
+  {
+    id: 1,
+    image: <IoFastFood />,
+    title: "Combos",
+    description: "Discover our wide range of burgers, pizzas, and smoothies.",
+    link: "/combos",
+  },
+  {
+    id: 2,
+    image: <RiDrinks2Fill />,
+    title: "Drinks",
+    description: "Experience our fresh and healthy options in our smoothie bar.",
+    link: "/drinks",
+  },
+  {
+    id: 3,
+    image: <LuDessert />,
+    title: "Desserts",
+    description: "Indulge in our delicious pizza toppings and crusts.",
+    link: "/desserts",
+  },
+  {
+    id: 4,
+    image: <FaBowlFood />,
+    title: "Main Dish",
+    description: "Get your favorite pasta cooked to perfection with our fresh ingredients.",
+    link: "/main-dishes",
+  }
+]
---
+++ src/components/Category.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { category } from "../app/assets/assets";
+import Link from "next/link";
+import Image from "next/image";
+
+const Category = () => {
+  return (
+    <div className="text-center py-10 px-5 lg:px-8 xl:px-[8%]">
+      <h2 className="font-Fruktur text-4xl">
+        Explore Our{" "}
+        <span className="text-primaryColor font-Fruktur">Categories</span>
+      </h2>
+      <p className="mt-4 text-sm">
+        Discover a variety of delicious dishes, crafted to satisfy every
+        craving.
+      </p>
+
+      <div className="flex gap-8 justify-center mt-10">
+        {category.map((category) => {
+          return (
+            <Link href={`${category.link}`} key={category.id}>
+              <div className="flip-card relative w-72 h-96">
+                {/* Card */}
+                <div className="flip-card-inner w-full h-full relative">
+                  {/* Front */}
+                  <div className="flip-card-front absolute w-full h-full flex flex-col items-center justify-center bg-white rounded-xl shadow-lg overflow-hidden">
+                    <div className="text-9xl text-primaryColor">
+                        {category.image}
+                    </div>
+                    <h3 className="text-lg text-[#DF9755] font-semibold mt-6">{category.title}</h3>
+                  </div>
+                  
+                  {/* Back */}
+                  <div className="flip-card-back absolute w-full h-full bg-primaryColor text-white flex items-center justify-center rounded-xl p-4 opacity-0">
+                    <p>{category.description}</p>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Category;
