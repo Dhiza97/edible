875a2ab9943186818b0e161478f71be1211b7c76
Joseph Garba <josephgarba97@gmail.com>
2025-02-04 19:53:13 +0100

    Update category section

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { category } from "../app/assets/assets";
 import Link from "next/link";
-import Image from "next/image";
 
 const Category = () => {
   return (
@@ -19,21 +18,19 @@ const Category = () => {
         {category.map((category) => {
           return (
             <Link href={`${category.link}`} key={category.id}>
-              <div className="flip-card relative w-full max-w-xs h-96 mx-auto">
-                {/* Card */}
-                <div className="flip-card-inner w-full h-full relative">
+              <div className="card-container">
+                <div className="card w-full sm:w-[300px]">
                   {/* Front */}
-                  <div className="flip-card-front absolute w-full h-full flex flex-col items-center justify-center bg-white rounded-xl shadow-lg overflow-hidden">
-                    <div className="text-9xl text-primaryColor">
+                  <div className="face front">
+                    <div className="icon text-9xl text-primaryColor">
                       {category.image}
                     </div>
                     <h3 className="text-lg text-[#DF9755] font-semibold mt-6">
                       {category.title}
                     </h3>
                   </div>
-
                   {/* Back */}
-                  <div className="flip-card-back absolute w-full h-full bg-primaryColor text-white flex items-center justify-center rounded-xl p-4 opacity-0">
+                  <div className="face back">
                     <p>{category.description}</p>
                   </div>
                 </div>
