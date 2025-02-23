ceee405c69f3d427ed6f54a02ca45fdca75bc272
Joseph Garba <josephgarba97@gmail.com>
2025-02-23 16:53:48 +0100

    Display featured products fro database

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -1,9 +1,16 @@
-import React from "react";
-import { products } from "../app/assets/assets";
+"use client"
+
+import React, { useContext } from "react";
 import Card from "./Card";
 import Link from "next/link";
+import { AppContext } from "../context/AppContext";
 
 const New = () => {
+  const { products } = useContext(AppContext);
+
+  // Filter products to get only those with isFeatured set to true
+  const featuredProducts = products.filter(product => product.isFeatured);
+
   return (
     <div className="text-center py-10 px-5 lg:px-8 xl:px-[8%] my-20">
       <h2 className="font-Fruktur text-4xl">
@@ -16,13 +23,13 @@ const New = () => {
 
       {/* Cards */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {products.slice(0, 6).map((product) => (
+        {featuredProducts.slice(0, 6).map((product) => (
           <Card key={product.id} product={product} />
         ))}
       </div>
 
       <Link href={'/menu'}>
-      <button className="py-4 px-8 border border-primaryColor text-primaryColor rounded-full hover:bg-primaryColor hover:text-white transition-all duration-300">See More</button>
+        <button className="py-4 px-8 border border-primaryColor text-primaryColor rounded-full hover:bg-primaryColor hover:text-white transition-all duration-300">See More</button>
       </Link>
     </div>
   );
