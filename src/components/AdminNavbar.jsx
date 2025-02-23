8b3629c8fa10b4cdc0b334ab33b645ed69dd233f
Joseph Garba <josephgarba97@gmail.com>
2025-02-23 15:17:19 +0100

    Add logout logic

--- src/components/AdminNavbar.jsx
+++ src/components/AdminNavbar.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import { assets } from '../app/assets/assets'
-import Image from 'next/image'
+"use client"
+
+import React from "react";
+import { assets } from "../app/assets/assets";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const AdminNavbar = () => {
-  return (
-    <div className='flex items-center py-2 px-[4%] justify-between'>
-      <Image className='w-[max(8%,70px)]' src={assets.logo_light} alt="" />
-      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs'>Logout</button>
-    </div>
-  )
-}
+  const router = useRouter();
 
-export default AdminNavbar
+  const handleLogout = () => {
+    localStorage.removeItem("adminToken");
+    router.push("/admin/login");
+  };
+
+  return (
+    <div className="flex items-center py-2 px-[4%] justify-between">
+      <Image className="w-[max(8%,70px)]" src={assets.logo_light} alt="" />
+      <button
+        className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs"
+        onClick={handleLogout}
+      >
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default AdminNavbar;
