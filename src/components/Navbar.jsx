c8a1288c3831fab51e1d7a984b0130119381b1af
Joseph Garba <josephgarba97@gmail.com>
2025-02-28 19:23:11 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -112,6 +112,14 @@ const Navbar = () => {
                 <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
                   <li>
                     <Link
+                      href="/orders"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700"
+                    >
+                      My Orders
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
                       href="/wishlist"
                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700"
                     >
